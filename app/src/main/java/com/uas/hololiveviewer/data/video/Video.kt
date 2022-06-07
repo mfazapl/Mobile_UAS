8f4ccd8fc7968a78b6694c6bc6411cdc81811e44
mfazapl <101613106+mfazapl@users.noreply.github.com>
2022-06-07 23:42:58 +0700

    Update Video.kt

--- app/src/main/java/com/uas/hololiveviewer/data/video/Video.kt
+++ app/src/main/java/com/uas/hololiveviewer/data/video/Video.kt
@@ -1,18 +1,46 @@
 package com.uas.hololiveviewer.data.video
 
 data class Video(
-    val bb_video_id: Any,
-    val channel: ChannelVideo,
-    val duration_secs: Any,
-    val id: Int,
-    val is_captioned: Any,
-    val is_uploaded: Any,
-    val live_end: Any,
-    val live_schedule: Any,
-    val live_start: Any,
-    val published_at: Any,
-    val status: String,
-    val thumbnail: Any,
-    val title: String,
-    val yt_video_key: String
+ 
+    @Json(name="bb_video_id")
+    val bb_video_id: Any? = null,
+    
+    @Json(name="channel")
+    val channel: ChannelVideo? = null,
+
+    @Json(name="duration_secs")
+    val duration_secs: Any? = null,
+
+    @Json(name="id")
+    val id: Int? =null,
+
+    @Json(name="is_captioned")
+    val is_captioned: Any? = null,
+
+    @Json(name="is_uploaded")
+    val is_uploaded: Any? =null,
+
+    @Json(name="live_end")
+    val live_end: Any? = null,
+
+    @Json(name="live_schedule")
+    val live_schedule: Any? = null,
+
+    @Json(name="live_start")
+    val live_start: Any? = null,
+
+    @Json(name="published_at")
+    val published_at: Any? = null,
+
+    @Json(name="status")
+    val status: String? = null,
+
+    @Json(name="thumbnail")
+    val thumbnail: Any? = null,
+
+    @Json(name="title")
+    val title: String? = null,
+
+    @Json(name="yt_video_key")
+    val yt_video_key: String? = null,
 )
