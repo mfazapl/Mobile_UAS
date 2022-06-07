bfb100a521e4716a4b1cd159d3b555eab2f7702f
mfazapl <101613106+mfazapl@users.noreply.github.com>
2022-06-07 23:44:41 +0700

    Update ChannelVideo.kt

--- app/src/main/java/com/uas/hololiveviewer/data/video/ChannelVideo.kt
+++ app/src/main/java/com/uas/hololiveviewer/data/video/ChannelVideo.kt
@@ -1,15 +1,36 @@
 package com.uas.hololiveviewer.data.video
 
 data class ChannelVideo(
-    val bb_space_id: Any,
-    val description: String,
-    val id: Int,
-    val name: String,
-    val photo: String,
-    val published_at: String,
-    val subscriber_count: Int,
-    val twitter_link: String,
-    val video_count: Int,
-    val view_count: Int,
-    val yt_channel_id: String
+     @Json (name="bb_space_id")
+    val bb_space_id: Any? = null,
+
+    @Json (name="description")
+    val description: String? = null,
+
+    @Json (name="id")
+    val id: Int? = null,
+
+    @Json (name="name")
+    val name: String? = null,
+
+    @Json (name="photo")
+    val photo: String? = null,
+
+    @Json (name="published_at")
+    val published_at: String? = null,
+
+    @Json (name="sucbcriber_count")
+    val subscriber_count: Int? = null,
+
+    @Json (name="twitter_link")
+    val twitter_link: String? = null,
+
+    @Json (name="video_count")
+    val video_count: Int? = null,
+
+    @Json(name="view_count")
+    val view_count: Int? = null,
+
+    @Json(name="yt_channel_id")
+    val yt_channel_id: String? = null
 )
